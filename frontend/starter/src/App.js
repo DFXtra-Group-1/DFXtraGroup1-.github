d6e5936203aa8f1fe9777d15d81e3c6fa19a0d1b
pranay-a-1 <pranay.a4795@gmail.com>
2022-08-18 02:18:17 +0100

    Header and Info with Scores part deriving data from mockdata file

--- frontend/starter/src/App.js
+++ frontend/starter/src/App.js
@@ -2,6 +2,8 @@ import './Styles/App.css';
 
 import YourProfile from './Components/profileView/YourProfile';
 import YourTraining from './Components/profileView/YourTraining';
+import Header from './Components/Header';
+import Info from './Components/information/Info';
 import mockData from './Tests/graduate-user-test.json';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
   return (
 
     <div className="App background">
+      <Header />
+      <Info graduates={mockData.graduateUser} />
       {/* <YourProfile graduates={mockData.graduateUser} /> */}
       <YourTraining graduates={mockData.graduateUser} />
     </div>
