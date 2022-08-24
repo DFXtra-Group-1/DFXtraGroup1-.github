b0409115cce0abcce9c839dcc88bb1e642ef5946
Pranay A <pranay.a4795@gmail.com>
2022-08-24 15:21:47 +0100

    test for trainingcard

--- frontend/starter/src/Components/profileView/TrainingCard.jsx
+++ frontend/starter/src/Components/profileView/TrainingCard.jsx
@@ -1,17 +1,32 @@
 const TrainingCard = () => {
 
-    const text = 'Module 1: Passed, Module 2: Passed, Module 3: Failed';
+    //const text = 'Module 1: Passed, Module 2: Passed, Module 3: Failed';
+
+    const moduleObj = {
+        Module1: "Passed",
+        Module2: "Passed",
+        Module3: "Failed",
+    }
+
 
     return (
         <div className="col-sm-6">
             <div className="card mx-2 my-5">
                 <div className="card-body" >
-                    <div class="form-group">
-                        <textarea class="form-control-plaintext" readonly id="exampleFormControlTextarea1" value={text} rows="3"></textarea>
+                    <div className="form-group">
+                        {/* <textarea class="form-control-plaintext" readonly id="exampleFormControlTextarea1" value={text} rows="3"></textarea> */}
+                        <div className="form-control-plaintext" readOnly id="exampleFormControlTextarea1" rows="3">
+                            <span> Module 1:</span>
+                            <span> {moduleObj.Module1}</span>
+                            <span> Module 2:</span>
+                            <span> {moduleObj.Module2}</span>
+                            <span> Module 3:</span>
+                            <span> {moduleObj.Module3}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 };
 
