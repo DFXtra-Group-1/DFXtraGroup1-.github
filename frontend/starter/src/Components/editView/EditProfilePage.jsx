cb2d70ed9dd5bf1099a92583bca2e2286821a92f
Rebecca Mpi <beckynmpi@gmail.com>
2022-08-24 10:40:10 +0100

    Updated edit profile view

--- frontend/starter/src/Components/editView/EditProfilePage.jsx
+++ frontend/starter/src/Components/editView/EditProfilePage.jsx
@@ -1,56 +1,126 @@
-import { useState, useEffect } from 'react'; 
-import { useParams } from "react-router-dom";
-import axios from 'axios'; 
-
 import PersonalStoryView from "./PersonalStory/PersonalStoryView"
+// import PersonalInformation from "./PersonalInformation";
+// import GraduateModel from "../utils/Graduate.model";
 
-const EditProfilePage = ( { SERVER_URL } ) => {
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-    const { uuid } = useParams();
+const EditProfilePage = ({ SERVER_URL }) => {
 
-    const [personalStory, setPersonalStory] = useState({
-        "_id": "",
-        "uuid": "",
-        "available": "",
-        "firstName": "",
-        "lastName": "",
-        "dateOfBirth": "",
-        "gender": "",
-        "nationality": "",
-        "personality": "",
-        "phone": "",
-        "linkedIn": "",
-        "gitHub": "",
-        "personalEmail": "",
-        "digitalFuturesEmail": "",
-        "degrees": [{}],
-        "schoolQualifications": [{}],
-        "workExperience": [{}],
-        "certificatesAndAwards": [{}],
-        "portfolio": [{}],
-        "personalSummary": "",
-        "cohort": "",
-        "learningPath": "",
-        "trainer": "",
-        "trainingFinishDate": ""
-    });
+    const { uuid } = useParams()
+
+    let _id = ``;
+    let uuid1 = ``;
+    let available = ``;
+    let dateOfBirth = ``;
+    let cohort = ``;
+    let learningPath = ``;
+    let trainer = ``;
+    let trainingFinishDate = ``;
+
+    const [firstName, setFirstName] = useState(``);
+    const [lastName, setLastName] = useState(``);
+    const [gender, setGender] = useState(``);
+    const [nationality, setNationality] = useState(``);
+    const [personality, setPersonality] = useState(``);
+    const [phone, setPhone] = useState(``);
+    const [linkedIn, setLinkedIn] = useState(``);
+    const [gitHub, setGitHub] = useState(``);
+    const [personalEmail, setPersonalEmail] = useState(``);
+    const [digitalFuturesEmail, setDigitalFuturesEmail] = useState(``);
+    const [degrees, setDegrees] = useState([{}]);
+    const [schoolQualifications, setSchoolQualifications] = useState([{}]);
+    const [workExperience, setWorkExperience] = useState([{}]);
+    const [certificatesAndAwards, setCertificatesAndAwards] = useState([{}]);
+    const [portfolio, setPortfolio] = useState([{}]);
+    const [personalSummary, setPersonalSummary] = useState(``);
+
+    const gradProps = {
+        firstName,
+        lastName,
+        personalEmail,
+        digitalFuturesEmail,
+        gitHub,
+        linkedIn,
+        phone,
+        gender,
+        nationality,
+        personality
+    };
+
+    const setProps = {
+        setFirstName,
+        setLastName,
+        setPersonalEmail,
+        setDigitalFuturesEmail,
+        setGitHub,
+        setLinkedIn,
+        setPhone,
+        setGender,
+        setNationality,
+        setPersonality
+    };
 
     const getData = async () => {
-
-        await axios.get(`${SERVER_URL}/graduate/${uuid}/`)
+        await axios.get(`${SERVER_URL}/graduate/${uuid}`)
             .then(res => {
-                console.log(res.data)
-                setPersonalStory(res.data);
+                setFirstName(res.data.firstName);
+                setLastName(res.data.lastName);
+                setPersonalEmail(res.data.personalEmail);
+                setDigitalFuturesEmail(res.data.digitalFuturesEmail);
+                setGitHub(res.data.gitHub);
+                setLinkedIn(res.data.linkedIn);
+                setPhone(res.data.phone);
+                setGender(res.data.gender);
+                setNationality(res.data.nationality);
+                setPersonality(res.data.personality);
+                setDegrees(res.data.degrees);
+                setSchoolQualifications(res.data.schoolQualifications);
+                setWorkExperience(res.data.workExperience);
+                setCertificatesAndAwards(res.data.certificatesAndAwards);
+                setPortfolio(res.data.portfolio);
+                setPersonalSummary(res.data.personalSummary);
+                _id = res.data._id;
+                uuid1 = res.data.uuid;
+                available = res.data.available;
+                dateOfBirth = res.data.dateOfBirth;
+                cohort = res.data.cohort;
+                learningPath = res.data.learningPath;
+                trainer = res.data.trainer;
+                trainingFinishDate = res.data.trainingFinishDate;
             })
-    }
-
+    };
     useEffect(() => {
         getData()
     }, []);
 
+    const submitHandler = e => {
+        e.preventDefault();
+        const graduateUpdated = new GraduateModel(_id, uuid1, available, firstName, lastName, dateOfBirth, gender, nationality, personality, phone, linkedIn, gitHub, personalEmail, digitalFuturesEmail, degrees, schoolQualifications, workExperience, certificatesAndAwards, portfolio, personalSummary, cohort, learningPath, trainer, trainingFinishDate)
 
-    return(
-        <PersonalStoryView personalStory={personalStory} />
+        putGraduate(graduateUpdated)
+    };
+
+    const putGraduate = async graduate => {
+        try {
+            await axios.put(`${SERVER_URL}/${uuid}`, graduate)
+        }
+        catch (err) {
+            alert(`Something went wrong: ${err.message}`)
+        }
+    };
+
+    return (
+        <>
+            {/* <form onSubmit={submitHandler}>
+                <button className="btn" type="submit">Submit draft</button>
+                <PersonalInformation gradProps={gradProps} setProps={setProps} />
+            </form> */}
+
+            <PersonalStoryView gradProps={gradProps} setProps={setProps}/> 
+        </>
     )
-}
-export default EditProfilePage; 
+};
+
+export default EditProfilePage;
